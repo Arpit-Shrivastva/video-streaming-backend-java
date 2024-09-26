0bb1fdd6be3e72b7413f0356282feae2f8fd0b8a
Arpit Shrivastava <152637028+Arpit-Shrivastva@users.noreply.github.com>
2024-09-26 08:48:56 +0530

    Add files via upload

---
+++ src/main/java/com/stream/app/Vide_Streaming_Backend/VideStreamingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stream.app.Vide_Streaming_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideStreamingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideStreamingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/app/Vide_Streaming_Backend/VideStreamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.app.Vide_Streaming_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideStreamingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
